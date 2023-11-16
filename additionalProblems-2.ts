6228daf43b8bc9ea536fd6ea0364160410756f22
Binay <binaykumarp@proximabiz.com>
2023-11-16 11:41:05 +0530

    generateIncome2 is without specialization, generateIncome is with specialization

--- additionalProblems-2.ts
+++ additionalProblems-2.ts
@@ -19,3 +19,13 @@ function generateIncome(sm: number): number[] {
 
     return income;
 }
+
+function generateIncome2(sm: number): number[] {
+    const ima = Array(12).fill(1)
+
+    const income = ima.map((_, index) => {
+        return index < sm - 1 ? 1 : 2;
+    });
+
+    return income;
+}
