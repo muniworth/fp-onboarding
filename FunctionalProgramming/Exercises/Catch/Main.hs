a68575874c505f5f2836722d8c14efa0075b5a1d
Robert Russell <robert@rr3.xyz>
2025-05-14 11:26:51 -0700

    FP intro: catch exercise

--- FunctionalProgramming/Exercises/Catch/Main.hs
+++ FunctionalProgramming/Exercises/Catch/Main.hs
@@ -1,4 +1,4 @@
-import Prelude hiding (fail, pure)
+import Prelude hiding (fail, pure, (>>=))
 
 data Fallible e a = Success a | Failure e deriving Show
 
@@ -8,19 +8,28 @@ pure = Success
 fail :: e -> Fallible e a
 fail = Failure
 
-bind :: Fallible e a -> (a -> Fallible e b) -> Fallible e b
-bind fa fb = case fa of
+(>>=) :: Fallible e a -> (a -> Fallible e b) -> Fallible e b
+(>>=) fa fb = case fa of
     Success a -> fb a
     Failure e -> fail e
+
+--------------------------------------------------------------------------------
 
 catch :: Fallible e a -> (e -> Fallible f a) -> Fallible f a
 catch = error "Not implemented!"
 
---test0, test1, test2 :: Fallible Int Int
-test0 :: Fallible Int Int
-test0 = catch (pure 0) \e -> pure 1
+--------------------------------------------------------------------------------
+
+test0, test1, test2, test3 :: Fallible String Int
+test0 = catch (pure 0) \_ -> pure 1
+test1 = catch (pure 0) \_ -> fail "B"
+test2 = catch (fail "A") \_ -> pure 1
+test3 = catch (fail "A") \_ -> fail "B"
 
 main :: IO ()
 main = do
     print test0
+    print test1
+    print test2
+    print test3
     return ()
