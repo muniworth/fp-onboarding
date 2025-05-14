8fe5269e577c29129d794d6c3497e23e8ce9a418
Robert Russell <robert@rr3.xyz>
2025-05-14 11:49:29 -0700

    FP intro: add GetAndSet exercise

---
+++ FunctionalProgramming/Exercises/GetAndSet/Main.hs
@@ -0,0 +1,31 @@
+import Prelude hiding (pure, (>>=))
+
+data State s a = State (s -> (a, s))
+
+pure :: a -> State s a
+pure a = State \s -> (a, s)
+
+runState :: State s a -> s -> (a, s)
+runState (State f) = f
+
+(>>=) :: State s a -> (a -> State s b) -> State s b
+sa >>= sb = State \s ->
+    case runState sa s of
+        (a, s') -> runState (sb a) s'
+
+--------------------------------------------------------------------------------
+
+get :: State s s
+get = error "Not implemented!"
+
+set :: s -> State s ()
+set = error "Not implemented!"
+
+--------------------------------------------------------------------------------
+
+test0, test1 :: State String String
+test0 = get
+test1 = set "B" >>= \() -> get
+
+main :: IO ()
+main = mapM_ (print . flip runState "A") [test0, test1]
